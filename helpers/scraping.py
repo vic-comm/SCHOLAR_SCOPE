5dad874731fff8107f98e4b82756676c00a10089
Victor Obiezue <obiezuechidera@gmail.com>
2025-09-05 17:31:29 +0100

    Scraping

--- helpers/scraping.py
+++ helpers/scraping.py
@@ -48,7 +48,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from bs4 import BeautifulSoup
+
 from datetime import datetime
 # Remove the decouple import since we're not using environment variables
 # from decouple import config
