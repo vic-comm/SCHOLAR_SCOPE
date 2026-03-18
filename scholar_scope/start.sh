ea9cb7364bcc5f1e61b671ebc3b1a91fc98647d0
Victor Chidera Obiezue <obiezuechidera@gmail.com>
2026-03-18 08:06:58 +0100

    removing celery from start.sh

--- scholar_scope/start.sh
+++ scholar_scope/start.sh
@@ -7,10 +7,6 @@ set -e
 echo "Applying migrations..."
 python manage.py migrate
 
-# 3. Start the Celery worker in the background
-echo "Starting Celery worker..."
-celery -A scholarscope worker -l info --concurrency=2 &
-
 # 4. Start Gunicorn and replace the shell process (exec)
 echo "Starting Gunicorn..."
 exec gunicorn scholarscope.wsgi:application --bind 0.0.0.0:$PORT --timeout 120 --workers 2 --log-level debug
